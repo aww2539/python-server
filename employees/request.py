d3783294baa9a1c34b9ef04e496ecc47921eb8be
Alex Woodall <aww2539@gmail.com>
2021-10-12 09:26:39 -0500

    added post functions for each component

--- employees/request.py
+++ employees/request.py
@@ -3,25 +3,25 @@ EMPLOYEES = [
         "id": 1,
         "name": "Luke Skywalker",
         "address": "333 Degoba System",
-        "location_id": 1
+        "locationId": 1
     },
     {
         "id": 2,
         "name": "Bart Simpson",
         "address": "90 Cartoon Way",
-        "location_id": 1
+        "locationId": 1
     },
     {
         "id": 3,
         "name": "Harry Potter",
         "address": "588 Hogwarts Stuff",
-        "location_id": 2
+        "locationId": 2
     },
     {
         "id": 4,
         "name": "Frodo Baggins",
         "address": "102 Somewhere in the shire blvd",
-        "location_id": 2
+        "locationId": 2
     }
 ]
 
@@ -42,3 +42,19 @@ def get_single_employee(id):
             requested_employee = employee
 
     return requested_employee
+
+def create_employee(employee):
+    # Get the id value of the last employee in the list
+    max_id = EMPLOYEES[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the employee dictionary
+    employee["id"] = new_id
+
+    # Add the employee dictionary to the list
+    EMPLOYEES.append(employee)
+
+    # Return the dictionary with `id` property added
+    return employee
