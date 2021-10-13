7055cdd0bbaf61e9acf8b3a21faddbed0c4f5b6e
Alex Woodall <aww2539@gmail.com>
2021-10-13 15:49:16 -0500

    added update function to animals

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,7 @@
+from functools import update_wrapper
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
@@ -160,7 +161,20 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_PUT(self):
         """Handles PUT requests to the server
         """
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def do_DELETE(self):
         self._set_headers(204)
